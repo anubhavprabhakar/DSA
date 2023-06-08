b937b65e150aa0339510d1469dd39c63414592c7
anubhavprabhakar <anu.devjob@gmail.com>
2023-06-09 03:43:18 +0530

    add comments in walk_matrix.py

--- array/walk_matrix.py
+++ array/walk_matrix.py
@@ -28,31 +28,34 @@ def walk_matrix(matrix):
   end_row=len(matrix)-1
   end_column=len(matrix[0])-1
   while(start_row<=end_row and start_column<=end_column):
-    
+    #traverse top row from left to right
     for column in range(start_column, end_column+1):
       num = matrix[start_row][column]
       elements.append(num)
-      
+    #increment start row by 1 as we traversed this row
     start_row+=1
-    
+    #traverse the right-most column from top to bottom
     for row in range(start_row, end_row+1):
         num = matrix[row][end_column]
         elements.append(num)
-  
+    #decrement the end column as we traversed this column
     end_column-=1
     
+    #check if the current row is visited or not, for edge case when only one row is left
     if(start_row<=end_row):
+      #traverse the bottom row from right to left
       for column in range(end_column, start_column-1, -1):
         num = matrix[end_row][column]
         elements.append(num)
-        
-    end_row-=1
+      #decrement the end row as we traversed this row    
+      end_row-=1
     
+    #check if the current column is visited or not, for edge case when only one column is left(one node technically)
     if(start_column<=end_column):
       for row in range(end_row, start_row-1, -1):
         num = matrix[row][start_column]
         elements.append(num)
-        
-    start_column+=1
+      #increment the start column as we traversed this column 
+      start_column+=1
   
   return elements
