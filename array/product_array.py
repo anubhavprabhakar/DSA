944a68961ec18203883463f2a9c658569f7b5701
anubhavprabhakar <anu.devjob@gmail.com>
2023-06-10 01:35:45 +0530

    add product_array solution

---
+++ array/product_array.py
@@ -0,0 +1,28 @@
+'''Given an array of integers, write a function build_product_array that returns an array where each item is the product of all the items in the input array except for the item at that index.
+
+Constraints:
+
+Solve this without using division
+You can create a results array, and it won't count against your space complexity
+Memory may be a concern though, so try to limit your use of additional data structures
+
+
+> take input array in list input
+> store solutions in list results[] and return it
+
+'''
+
+# @param {List [int]} input
+# @return {List [int]}
+
+def build_product_array(input):
+  results = []
+  
+  for i in range(len(input)):
+    product = 1
+    for j in range(len(input)):
+      if j==i:
+        continue
+      product*=input[j]
+    results.append(product)
+  return results
