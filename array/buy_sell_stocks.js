73dd5fbb1fab8706ad8c1dba55c89800e50c081e
anubhavprabhakar <anu.devjob@gmail.com>
2023-06-13 02:08:41 +0530

    add buy_sell_stocks.js

---
+++ array/buy_sell_stocks.js
@@ -0,0 +1,19 @@
+/** link to problem: https://skilled.dev/course/best-time-to-buy-and-sell-stock
+
+ * @param {number[]} prices
+ * @return {number}
+ */
+const getMaxProfit = (prices) => {
+  var max_profit = 0
+  
+  for(let i=0; i<prices.length-1; i++){
+    for(let j=i+1; j<prices.length; j++){
+      let profit = prices[j]-prices[i]
+      if(max_profit < profit){
+        max_profit = profit
+      }
+    }
+  }
+  
+  return max_profit
+};
