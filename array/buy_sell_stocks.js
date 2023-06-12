7e3309832c1e0455d80c8109bfd38aec86bd1640
anubhavprabhakar <anu.devjob@gmail.com>
2023-06-13 03:09:11 +0530

    add optimal solution buy_sell_stocks.js

--- array/buy_sell_stocks.js
+++ array/buy_sell_stocks.js
@@ -1,19 +1,23 @@
 /** link to problem: https://skilled.dev/course/best-time-to-buy-and-sell-stock
 
-This is the brute force solution with time complexity of O(n^2)
+This is the optimal solution with time complexity of O(n)
+It uses a greedy algorithm to track the lowest and heighest price at the current day,
+and calculates the difference between the smallest price encountered till now 
+and the current price, inorder to find the max profit.
 
 * @param {number[]} prices
 * @return {number}
 */
 const getMaxProfit = (prices) => {
   var max_profit = 0
+  var min_price = prices[0]
   
-  for(let i=0; i<prices.length-1; i++){
-    for(let j=i+1; j<prices.length; j++){
-      let profit = prices[j]-prices[i]
-      if(max_profit < profit){
-        max_profit = profit
-      }
+  for(let i=0; i<prices.length; i++){
+    if(prices[i]<=min_price){
+      min_price=prices[i]
+    }else{
+      var profit = prices[i]-min_price
+      max_profit = Math.max(profit, max_profit)
     }
   }
   
